21182929b217c21a1ea669533015a1c61a4b2e50
Joaquin Fox <joaquin.fox@gmail.com>
2018-10-31 10:24:33 -0700

    key deleter

--- objectDrills.js
+++ objectDrills.js
@@ -39,4 +39,21 @@ function personMaker() {
   return person;
 }
 
-console.log(personMaker());
+//console.log(personMaker());
+
+//key deleter
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj){
+    delete obj.foo;
+    delete obj.bar;
+    return obj;
+}
+
+console.log(keyDeleter(sampleObj));
