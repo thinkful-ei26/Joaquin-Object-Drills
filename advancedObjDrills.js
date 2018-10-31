c0afe0327c2648a2308934c975651717f16dd1b4
Joaquin Fox <joaquin.fox@gmail.com>
2018-10-31 11:37:40 -0700

    enroll in summer school

--- advancedObjDrills.js
+++ advancedObjDrills.js
@@ -6,4 +6,32 @@ function makeStudentReports(data){
 
 }
 
-console.log(makeStudentReports(sampleData));
+//console.log(makeStudentReports(sampleData));
+
+//enroll in summer school
+
+const sampleStudent = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology'
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics'
+  }
+];
+
+function enrollInSummerSchool(students) {
+    return students.map(student => {
+        return {
+            name: student.name,
+            status: "In Summer school",
+            course: student.course
+           }
+        })
+    };
+
+
+console.log(enrollInSummerSchool(sampleStudent));
