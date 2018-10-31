44a1484b77b93bed7ff682b96aba22346fc63417
Joaquin Fox <joaquin.fox@gmail.com>
2018-10-31 10:11:17 -0700

    updateObject drill

--- objectDrills.js
+++ objectDrills.js
@@ -1,14 +1,28 @@
 //Object creator
-function createMyObject (){
-    return {
-        foo: 'bar',
-        answerToUniverse : 42,
-        'olly olly' : 'oxen free',
-        sayHello: function(){
-            return 'Hello';
-        }
-    };
+// function createMyObject (){
+//     return {
+//         foo: 'bar',
+//         answerToUniverse : 42,
+//         'olly olly' : 'oxen free',
+//         sayHello: function(){
+//             return 'Hello';
+//         }
+//     };
         
-}
+// }
 
-console.log(createMyObject());
+//console.log(createMyObject());
+
+//Object updater
+
+let obj = {};
+
+function updateObject(obj) {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
+
+}
+console.log(updateObject(obj));
